04cf26895f3c06131810fed1297e1215ebf9b192
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-26 09:41:30 +0100

    Adding the third command

---
+++ temperature.js
@@ -0,0 +1,7 @@
+function celciustoFahrenheit(celcius) {
+    return (celcius * 9/5) + 32
+}
+let fahrenheitOne = celciustoFahrenheit(0);
+let fahrenheitTwo = celciustoFahrenheit(100);
+console.log(fahrenheitOne)
+console.log(fahrenheitTwo)
