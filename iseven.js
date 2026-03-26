003cfff18f3a3cf8b951715bd942a1d95bb1b5ff
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-26 03:11:28 +0100

    Adding the second command

---
+++ iseven.js
@@ -0,0 +1,11 @@
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let firstNumber = isEven(4);
+let secondNumber = isEven(7);
+console.log(firstNumber)
+console.log(secondNumber)
